494106ff93a8f1e2d87831db3cdb1fd2e7da3c6b
Rigel Ramadhani Waloni <rigel8911@gmail.com>
2023-11-28 18:58:29 +0700

    feat: Adding other members

--- src/membersData.js
+++ src/membersData.js
@@ -6,6 +6,7 @@ import NRP5 from './assets/PhotoMember/5.png';
 import NRP6 from './assets/PhotoMember/6.png';
 import NRP7 from './assets/PhotoMember/7.png';
 import NRP8 from './assets/PhotoMember/8.png';
+
 import NRP9 from './assets/PhotoMember/9.png';
 import NRP10 from './assets/PhotoMember/10.png';
 import NRP11 from './assets/PhotoMember/11.png';
@@ -14,7 +15,63 @@ import NRP13 from './assets/PhotoMember/13.png';
 import NRP14 from './assets/PhotoMember/14.png';
 import NRP15 from './assets/PhotoMember/15.png';
 import NRP16 from './assets/PhotoMember/16.png';
+
 import NRP17 from './assets/PhotoMember/17.png';
+import NRP18 from './assets/PhotoMember/18.png';
+import NRP19 from './assets/PhotoMember/19.png';
+import NRP20 from './assets/PhotoMember/20.png';
+import NRP21 from './assets/PhotoMember/21.png';
+import NRP22 from './assets/PhotoMember/22.png';
+import NRP23 from './assets/PhotoMember/23.png';
+import NRP25 from './assets/PhotoMember/25.png';
+
+import NRP26 from './assets/PhotoMember/26.png';
+import NRP29 from './assets/PhotoMember/29.png';
+import NRP30 from './assets/PhotoMember/30.png';
+import NRP31 from './assets/PhotoMember/31.png';
+import NRP32 from './assets/PhotoMember/32.png';
+import NRP34 from './assets/PhotoMember/34.png';
+import NRP35 from './assets/PhotoMember/35.png';
+import NRP36 from './assets/PhotoMember/36.png';
+
+import NRP37 from './assets/PhotoMember/37.png';
+import NRP38 from './assets/PhotoMember/38.png';
+import NRP39 from './assets/PhotoMember/39.png';
+import NRP40 from './assets/PhotoMember/40.png';
+import NRP41 from './assets/PhotoMember/41.png';
+import NRP42 from './assets/PhotoMember/42.png';
+import NRP44 from './assets/PhotoMember/44.png';
+import NRP45 from './assets/PhotoMember/45.png';
+
+import NRP46 from './assets/PhotoMember/46.png';
+import NRP47 from './assets/PhotoMember/47.png';
+import NRP48 from './assets/PhotoMember/48.png';
+import NRP49 from './assets/PhotoMember/49.png';
+import NRP50 from './assets/PhotoMember/50.png';
+import NRP51 from './assets/PhotoMember/51.png';
+import NRP52 from './assets/PhotoMember/52.png';
+import NRP53 from './assets/PhotoMember/53.png';
+
+import NRP54 from './assets/PhotoMember/54.png';
+import NRP56 from './assets/PhotoMember/56.png';
+import NRP57 from './assets/PhotoMember/57.png';
+import NRP58 from './assets/PhotoMember/58.png';
+import NRP59 from './assets/PhotoMember/59.png';
+import NRP60 from './assets/PhotoMember/60.png';
+import NRP61 from './assets/PhotoMember/61.png';
+import NRP63 from './assets/PhotoMember/63.png';
+
+import NRP64 from './assets/PhotoMember/64.png';
+import NRP65 from './assets/PhotoMember/65.png';
+import NRP66 from './assets/PhotoMember/66.png';
+import NRP67 from './assets/PhotoMember/67.png';
+import NRP68 from './assets/PhotoMember/68.png';
+import NRP69 from './assets/PhotoMember/69.png';
+import NRP70 from './assets/PhotoMember/70.png';
+import NRP71 from './assets/PhotoMember/71.png';
+
+import NRP73 from './assets/PhotoMember/73.png';
+import NRP74 from './assets/PhotoMember/74.png';
 
 const members = [
   { id: 0, name: 'Rezky Dwisantika Pujiastuti', nrp: '5024221001', photo: NRP1 },
@@ -36,13 +93,61 @@ const members = [
   { id: 15, name: 'Sila Ulfania', nrp: '5024221016', photo: NRP16 },
 
   { id: 16, name: 'Ahmad Akmal Rijal', nrp: '5024221017', photo: NRP17 },
-  { id: 17, name: 'Muhamad Risqi Aditiya', nrp: '5024221010', photo: NRP10 },
-  { id: 18, name: 'Farrell Rafee Sudjatmiko', nrp: '5024221011', photo: NRP11 },
-  { id: 19, name: 'Yohana Magdalena Franklin Harianja', nrp: '5024221012', photo: NRP12 },
-  { id: 20, name: 'Muhammad Ihsan Alfarisi', nrp: '5024221013', photo: NRP13 },
-  { id: 21, name: 'Muhamad Rizano Alamsyah', nrp: '5024221014', photo: NRP14 },
-  { id: 22, name: 'Cedric Anthony Edysa', nrp: '5024221015', photo: NRP15 },
-  { id: 23, name: 'Sila Ulfania', nrp: '5024221016', photo: NRP16 },
+  { id: 17, name: 'Zadun Nafiq', nrp: '5024221018', photo: NRP18 },
+  { id: 18, name: 'Miftah Ghifari', nrp: '5024221019', photo: NRP19 },
+  { id: 19, name: 'Yuvina Rahma Ramadhani', nrp: '5024221020', photo: NRP20 },
+  { id: 20, name: 'Agus Fuad Mudhofar', nrp: '5024221021', photo: NRP21 },
+  { id: 21, name: 'Mukhamad Nabila Pratama', nrp: '5024221022', photo: NRP22 },
+  { id: 22, name: 'Hendrich Ardthian Breshman Panjaitan', nrp: '5024221023', photo: NRP23 },
+  { id: 23, name: 'Larasati Lituhayu', nrp: '5024221025', photo: NRP25 },
+
+  { id: 24, name: 'Mahija Ibad Pradipta', nrp: '5024221026', photo: NRP26 },
+  { id: 25, name: 'Muhammad Azzikri Putra Wardani', nrp: '5024221029', photo: NRP29 },
+  { id: 26, name: 'Muhammad Haekal Muhyidin Al-Araby', nrp: '5024221030', photo: NRP30 },
+  { id: 27, name: 'Raditya Rakha Renanda', nrp: '5024221031', photo: NRP31 },
+  { id: 28, name: 'Defender Artha Widiprasetyo', nrp: '5024221032', photo: NRP32 },
+  { id: 29, name: 'Alif Ridwan Al Ghozali', nrp: '5024221034', photo: NRP34 },
+  { id: 30, name: 'Ahmad Aqbil Naim', nrp: '5024221035', photo: NRP35 },
+  { id: 31, name: 'Edrickson Edgar', nrp: '5024221036', photo: NRP36 },
+  
+  { id: 32, name: 'Nabila Mutiara Susetio', nrp: '5024221037', photo: NRP37 },
+  { id: 33, name: 'Azaria Putri Fawnia', nrp: '5024221038', photo: NRP38 },
+  { id: 34, name: 'M. Al Hakiim Fakhri Sultansyah', nrp: '5024221039', photo: NRP39 },
+  { id: 35, name: 'Athlon Hari Sulistyo', nrp: '5024221040', photo: NRP40 },
+  { id: 36, name: 'Muhammad Syawal Ridho Fadhillah', nrp: '5024221041', photo: NRP41 },
+  { id: 37, name: 'Hasan Palito', nrp: '5024221042', photo: NRP42 },
+  { id: 38, name: 'Aji Wirasandya Pratama', nrp: '5024221044', photo: NRP44 },
+  { id: 39, name: 'Nathanael Timotius Siregar', nrp: '5024221045', photo: NRP45 },
+  
+  { id: 40, name: 'Rizki Farhan Nabil', nrp: '5024221046', photo: NRP46 },
+  { id: 41, name: 'Surya Pebrianto', nrp: '5024221047', photo: NRP47 },
+  { id: 42, name: 'Mochammad Gery Tauristino', nrp: '5024221048', photo: NRP48 },
+  { id: 43, name: 'Daniel Oliver Enoch Bachtiar', nrp: '5024221049', photo: NRP49 },
+  { id: 44, name: 'Farhan Abdurrahman Muthohhar', nrp: '5024221050', photo: NRP50 },
+  { id: 45, name: 'Omar Muhammad Gibran', nrp: '5024221051', photo: NRP51 },
+  { id: 46, name: 'Pranaditya Tri Jyotista Vavitram Putra Sudana', nrp: '5024221052', photo: NRP52 },
+  { id: 47, name: 'Muhammad Mirza Farisy', nrp: '5024221053', photo: NRP53 },
+  
+  { id: 48, name: 'Barra Alastair Prayata', nrp: '5024221054', photo: NRP54 },
+  { id: 49, name: 'Muhammad Naufal Irsha Ananda', nrp: '5024221056', photo: NRP56 },
+  { id: 50, name: 'I Gusti Ngurah Opaldi Partha', nrp: '5024221057', photo: NRP57 },
+  { id: 51, name: 'Rigel Ramadhani Waloni', nrp: '5024221058', photo: NRP58 },
+  { id: 52, name: 'Hilmy Hanindia Nugroho', nrp: '5024221059', photo: NRP59 },
+  { id: 53, name: 'Justin Christopher Tetelepta', nrp: '5024221060', photo: NRP60 },
+  { id: 54, name: 'Alhadiid Muhammad Aufaa Prayitno', nrp: '5024221061', photo: NRP61 },
+  { id: 55, name: 'Rama Prawira Suryanto', nrp: '5024221063', photo: NRP63 },
+  
+  { id: 56, name: 'Ahmad Wildan Syahputra', nrp: '5024221064', photo: NRP64 },
+  { id: 57, name: 'Andika Fathurrahman Achiral', nrp: '5024221065', photo: NRP65 },
+  { id: 58, name: 'Imanuel Daulat Satrio Utomo Siahaan', nrp: '5024221066', photo: NRP66 },
+  { id: 59, name: 'Adhitya Raufarhan Sukmana', nrp: '5024221067', photo: NRP67 },
+  { id: 60, name: 'Mikha Arlen Imanuel Prastowo', nrp: '5024221068', photo: NRP68 },
+  { id: 61, name: 'Vania Bunga Febrina', nrp: '5024221069', photo: NRP69 },
+  { id: 62, name: 'Hibban Rabbani Salni', nrp: '5024221070', photo: NRP70 },
+  { id: 63, name: 'Syahmirza Ahmad Ramadhani', nrp: '5024221071', photo: NRP71 },
+  
+  { id: 64, name: 'Xyz Frizy Firstyaji', nrp: '5024221073', photo: NRP73 },
+  { id: 65, name: 'Arde Wondiwoy', nrp: '5024221074', photo: NRP74 },
 ];
 
 export default members;
