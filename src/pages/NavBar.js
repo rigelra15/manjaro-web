1678fdce3b66f0c683523e0b8d44e05ff832db96
Rigel Ramadhani Waloni <rigel8911@gmail.com>
2023-11-28 21:36:28 +0700

    feat: Add search bar animation

--- src/pages/NavBar.js
+++ src/pages/NavBar.js
@@ -1,20 +1,38 @@
 import React, { useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import logoManjaro from '../assets/logo-manjaro.png';
-import { FaMagnifyingGlass, FaBars, FaMoon } from 'react-icons/fa6';
+import { FaMagnifyingGlass, FaBars } from 'react-icons/fa6';
+
+import members from '../membersData';
 
 const NavBar = () => {
   const location = useLocation();
   const [menuOpen, setMenuOpen] = useState(false);
-  const [darkMode, setDarkMode] = useState(false);
+  const [searchInput, setSearchInput] = useState('');
+  const [filteredMembers, setFilteredMembers] = useState(members);
+  const [searchInputClass, setSearchInputClass] = useState('');
+  const [searchInputWidth, setSearchInputWidth] = useState('200px'); // Initial width of the search input
 
   const toggleMenu = () => {
     setMenuOpen(!menuOpen);
   };
 
-  const toggleDarkMode = () => {
-    setDarkMode(!darkMode);
-    document.body.classList.toggle('dark');
+  const handleSearch = () => {
+    const searchTerm = searchInput.toLowerCase();
+    const filtered = members.filter((member) =>
+      member.name.toLowerCase().includes(searchTerm) || member.nrp.includes(searchTerm)
+    );
+    setFilteredMembers(filtered);
+  };
+
+  const handleSearchFocus = () => {
+    setSearchInputClass('focus');
+    setSearchInputWidth('300px');
+  };
+
+  const handleSearchBlur = () => {
+    setSearchInputClass('');
+    setSearchInputWidth('200px');
   };
 
   return (
@@ -36,105 +54,69 @@ const NavBar = () => {
           } lg:flex absolute inset-0 flex items-center justify-center`}
         >
           <ul className="flex lg:visible lg:space-x-2 invisible md:space-x-0 items-center justify-center">
-            <li className={`px-4 py-1 rounded-full ${location.pathname === '/' ? 'bg-[#DC143C] text-white' : 'text-black hover:bg-gray-300 transition-transform duration-500'}`}>
+            <li
+              className={`px-4 py-1 rounded-full ${
+                location.pathname === '/' ? 'bg-[#DC143C] text-white' : 'text-black hover:bg-gray-300 transition-transform duration-500'
+              }`}
+            >
               <Link to="/">Home</Link>
             </li>
-            <li className={`px-4 py-1 rounded-full ${location.pathname === '/member' ? 'bg-[#49B8D3] text-white' : 'text-black hover:bg-gray-300 transition-transform duration-500'}`}>
+            <li
+              className={`px-4 py-1 rounded-full ${
+                location.pathname === '/member' ? 'bg-[#49B8D3] text-white' : 'text-black hover:bg-gray-300 transition-transform duration-500'
+              }`}
+            >
               <Link to="/member">Member</Link>
             </li>
-            <li className={`px-4 py-1 rounded-full ${location.pathname === '/achievement' ? 'bg-[#FFD801] text-white' : 'text-black hover:bg-gray-300 transition-transform duration-500'}`}>
+            <li
+              className={`px-4 py-1 rounded-full ${
+                location.pathname === '/achievement' ? 'bg-[#FFD801] text-white' : 'text-black hover:bg-gray-300 transition-transform duration-500'
+              }`}
+            >
               <Link to="/achievement">Achievement</Link>
             </li>
-            <li className={`px-4 py-1 rounded-full ${location.pathname === '/profile' ? 'bg-[#0066CC] text-white' : 'text-black hover:bg-gray-300 transition-transform duration-500'}`}>
+            <li
+              className={`px-4 py-1 rounded-full ${
+                location.pathname === '/profile' ? 'bg-[#0066CC] text-white' : 'text-black hover:bg-gray-300 transition-transform duration-500'
+              }`}
+            >
               <Link to="/profile">Profile</Link>
             </li>
           </ul>
         </div>
         <div className="mr-5 lg:mr-20 relative flex flex-row items-center justify-center gap-5">
-          <div>
-            <div className="absolute inset-y-0 right-0 flex items-center pr-5 pointer-events-none">
-              <FaMagnifyingGlass className="text-gray-400" />
-            </div>
+          <div
+            className={`transition-all duration-500 flex flex-row ${
+              location.pathname !== '/member' ? 'visible' : 'invisible'
+            } ${searchInputClass}`}
+            style={{ width: searchInputWidth }}
+          >
             <input
               type="text"
               placeholder="Search"
-              style={{ width: 150 }}
+              value={searchInput}
+              onChange={(e) => setSearchInput(e.target.value)}
+              onFocus={handleSearchFocus}
+              onBlur={handleSearchBlur}
               className="p-2 pl-5 border-2 rounded-full"
+              style={{ width: '100%' }}
             />
-          </div>
-          {/* <div className="mr-5 lg:mr-20">
-            <button onClick={toggleDarkMode} className={`p-2 ${darkMode ? 'text-white' : 'text-black'}`}>
-              {darkMode ? 'Light' : 'Dark'} Mode
+            <button
+              onClick={handleSearch}
+              className="ml-2 px-3 py-3 bg-gray-300 hover:bg-blue-500 text-white rounded-full"
+            >
+              <FaMagnifyingGlass />
             </button>
-          </div> */}
+          </div>
         </div>
       </nav>
       <style>
-        @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@500&display=swap');
         {`
-        /* The switch - the box around the slider */
-        .switch {
-          position: relative;
-          display: inline-block;
-          width: 46px;
-          height: 18px;
-        }
-        
-        /* Hide default HTML checkbox */
-        .switch input {
-          opacity: 0;
-          width: 0;
-          height: 0;
-        }
-        
-        /* The slider */
-        .slider {
-          position: absolute;
-          cursor: pointer;
-          top: 0;
-          left: 0;
-          right: 0;
-          bottom: 0;
-          background-color: #ccc;
-          -webkit-transition: .4s;
-          transition: .4s;
-        }
-        
-        .slider:before {
-          position: absolute;
-          content: "";
-          height: 26px;
-          width: 26px;
-          left: -4px;
-          bottom: -4px;
-          background-color: darkgray;
-          -webkit-transition: .4s;
-          transition: .4s;
-        }
-        
-        input:checked + .slider {
-          background-color: #2196F3;
-        }
-        
-        input:focus + .slider {
-          box-shadow: 0 0 1px #2196F3;
-        }
-        
-        input:checked + .slider:before {
-          -webkit-transform: translateX(26px);
-          -ms-transform: translateX(26px);
-          transform: translateX(26px);
-          background-color: darkblue;
-        }
-        
-        /* Rounded sliders */
-        .slider.round {
-          border-radius: 34px;
-        }
-        
-        .slider.round:before {
-          border-radius: 50%;
-        }`}
+          @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@500&display=swap');
+          .focus {
+            width: 250px; /* Set the width when the input is focused */
+          }
+        `}
       </style>
     </>
   );
